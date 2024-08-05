faab95e636a388b1b7353e33cf845f993637ecd2
黄智豪 <huangzhihao@galasports.com>
2024-08-05 19:23:44 +0800

    test

--- test/build.groovy
+++ test/build.groovy
@@ -1,14 +1,7 @@
-//node { // 加入 node 块以提供必要的 FilePath 上下文
-//    def utils = load 'test/utils.groovy'
-//    stage("say Hello") {
-//        utils.sayHello()
-//    }
-//
-//}
-//
-//def greetingOutside = utils.sayHello("World")
-//println(greetingOutside)
-utils = this.class.classLoader.parseClass("utils.groovy")
+
+def utilsClass = this.class.classLoader.parseClass(new File('utils.groovy'))
+def utils = utilsClass.newInstance() // 创建 utils 类的实例
+
 properties([
     parameters([
     // 平台选择参数选项 PublishPlatform
@@ -25,9 +18,6 @@ pipeline {
             steps {
                 script {
                     utils.sayHello()
-
-//                    def greeting = utils.sayHello("World")
-//                    println(greeting)
 
 
                 }
