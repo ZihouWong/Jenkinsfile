f77c1fc0b7737782efc73ffd2a73e7e24e71d668
黄智豪 <huangzhihao@galasports.com>
2024-08-08 18:52:53 +0800

    test

--- test/demo/build.groovy
+++ test/demo/build.groovy
@@ -20,11 +20,19 @@ pipeline {
         stage('Prepare') {
             steps {
                 script {
-                    echo "environment"
+                    // 加载 Utils.groovy 文件并获取实例
                     env.utils = load 'Utils/utils.groovy'
+
+                    // 检查 env.utils 是否正确
+                    echo "Utils instance loaded: ${env.utils}"
+
+                    // 获取选择的平台
                     def selectedPlatform = params.PublishPlatform
                     echo "Selected platform: ${selectedPlatform}"
+
+                    // 调用 getHomePath 方法
                     env.HOME = env.utils.getHomePath(selectedPlatform)
+                    echo "Home path: ${env.HOME}"
 
                 }
                 script {
