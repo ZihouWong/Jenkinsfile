06e98757289f80c9470a4fe431adcfa2f33cf195
黄智豪 <huangzhihao@galasports.com>
2024-08-08 19:06:07 +0800

    tes

--- test/demo/build.groovy
+++ test/demo/build.groovy
@@ -1,6 +1,12 @@
 
 
 
+
+def getUtils() {
+    def utils = load 'Utils/utils.groovy'
+    return utils
+}
+
 properties([
         parameters(
                 [
@@ -11,30 +17,17 @@ properties([
 ])
 
 pipeline {
+    environment {
+        utilsObject = getUtils()
 
+        HOME = utilsObject.getHomePath("$params.PublishPlafrom}")
+    }
 
     agent any
 
     stages {
-
         stage('Prepare') {
             steps {
-                script {
-                    // 加载 Utils.groovy 文件并获取实例
-                    env.utils = load 'Utils/utils.groovy'
-
-                    // 检查 env.utils 是否正确
-                    echo "Utils instance loaded: ${env.utils}"
-
-                    // 获取选择的平台
-                    def selectedPlatform = params.PublishPlatform
-                    echo "Selected platform: ${selectedPlatform}"
-
-                    // 调用 getHomePath 方法
-                    env.HOME = env.utils.getHomePath(selectedPlatform)
-                    echo "Home path: ${env.HOME}"
-
-                }
                 script {
                     echo "HOME: ${env.HOME}"
                     def demoConfig = load 'test/demo/demoConfig.groovy'
@@ -46,8 +39,8 @@ pipeline {
 //                    echo "The name of the utility is: ${utils.name}"
 ////
 ////
-//                    echo "${Utils.sayHello("Wong")}"
-
+                    echo "${env.utils.sayHello("Wong")}"
+                    echo "${utils.sayHello("Wong")}"
 
 
                 }
